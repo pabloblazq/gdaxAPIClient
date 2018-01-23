6fd1d6e004d4b21a98779d190adf0e22b622ea5c
pablo.blazquez <pablo.blazquez@openjawtech.com>
2018-01-23 15:56:41 +0100

    Integrate Gson transformation inside the TradesResource (pending to do it inside the rest of Resources)

--- src/main/java/com/blame/gdaxAPIClient/Test.java
+++ src/main/java/com/blame/gdaxAPIClient/Test.java
@@ -1,22 +1,24 @@
 package com.blame.gdaxAPIClient;
 
+import java.util.Collection;
+
 import com.blame.gdaxAPIClient.market.book.Book;
 import com.blame.gdaxAPIClient.market.book.BookResource;
 import com.blame.gdaxAPIClient.market.trades.Trade;
 import com.blame.gdaxAPIClient.market.trades.TradesResource;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
+		
 		TradesResource r = new TradesResource("BTC-EUR");
-		Gson gson = new Gson();
-		String response = r.get();
-		Trade[] trades = gson.fromJson(response, Trade[].class);
+		Collection<Trade> trades = r.getTrades();
 //		bb.normalize();
-		String response2 = r.next();
-		Trade[] trades2 = gson.fromJson(response2, Trade[].class);
+//		Thread.sleep(10000);
+		Collection<Trade> trades2 = r.getTradesOlder();
 		System.out.println(trades2);
 	}
 
--- src/main/java/com/blame/gdaxAPIClient/market/trades/TradesResource.java
+++ src/main/java/com/blame/gdaxAPIClient/market/trades/TradesResource.java
@@ -1,7 +1,11 @@
 package com.blame.gdaxAPIClient.market.trades;
 
+import java.lang.reflect.Type;
+import java.util.Collection;
+
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -9,49 +13,78 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.blame.gdaxAPIClient.GdaxAPIConstants;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class TradesResource {
 	private static final Logger logger = LogManager.getLogger(TradesResource.class);
 	
 	protected static final String RESOURCE_PATH_PRODUCTS = "products";
 	protected static final String RESOURCE_PATH_BOOK = "trades";
-	
+
+	protected static Type tradeCollectionType = new TypeToken<Collection<Trade>>(){}.getType();
+
 	protected Invocation.Builder ib;
+	protected WebTarget wt;
 	protected String product;
 	
-	protected String cursorForOlder;
+	protected String cursorBefore;
+	protected String cursorAfter;
 	
+	protected Gson gson = new Gson();
+
 	public TradesResource(String product) {
 		super();
 		this.product = product;
 
 		logger.info("Building resource for " + this.getClass().getSimpleName() + " ...");
-		ib = ClientBuilder
+		wt = ClientBuilder
 				.newClient()
 				.target(GdaxAPIConstants.GDAX_API_ENDPOINT_URL)
-				.path(RESOURCE_PATH_PRODUCTS).path(product).path(RESOURCE_PATH_BOOK)
-				.request(MediaType.APPLICATION_JSON);
+				.path(RESOURCE_PATH_PRODUCTS).path(product).path(RESOURCE_PATH_BOOK);
+		ib = wt.request(MediaType.APPLICATION_JSON);
 	}
 	
-	public String get() {
+	public Collection<Trade> getTrades() {
 		logger.info("Sending GET request over the resource...");
-		Response response = this.ib.get();
-		this.cursorForOlder = response.getHeaderString("CB-AFTER");
-		String sResponse = response.readEntity(String.class);
-		return sResponse;
-	}
-	
-	public String next() {
-		Invocation.Builder tempIb = ClientBuilder
-				.newClient()
-				.target(GdaxAPIConstants.GDAX_API_ENDPOINT_URL)
-				.path(RESOURCE_PATH_PRODUCTS).path(product).path(RESOURCE_PATH_BOOK)
-				.queryParam("after", cursorForOlder)
-				.request(MediaType.APPLICATION_JSON);
 
 		Response response = this.ib.get();
-		this.cursorForOlder = response.getHeaderString("CB-AFTER");
+
+		this.cursorBefore = response.getHeaderString("CB-BEFORE");
+		this.cursorAfter = response.getHeaderString("CB-AFTER");
+		
 		String sResponse = response.readEntity(String.class);
-		return sResponse;
+
+		return gson.fromJson(sResponse, tradeCollectionType);
+	}
+	
+	public Collection<Trade> getTradesNewer() {
+		Invocation.Builder tempIb = wt
+				.queryParam("before", cursorBefore)
+				.request(MediaType.APPLICATION_JSON);
+
+		Response response = tempIb.get();
+
+		this.cursorBefore = response.getHeaderString("CB-BEFORE");
+		this.cursorAfter = response.getHeaderString("CB-AFTER");
+
+		String sResponse = response.readEntity(String.class);
+
+		return gson.fromJson(sResponse, tradeCollectionType);
+	}
+
+	public Collection<Trade> getTradesOlder() {
+		Invocation.Builder tempIb = wt
+				.queryParam("after", cursorAfter)
+				.request(MediaType.APPLICATION_JSON);
+
+		Response response = tempIb.get();
+
+		this.cursorBefore = response.getHeaderString("CB-BEFORE");
+		this.cursorAfter = response.getHeaderString("CB-AFTER");
+
+		String sResponse = response.readEntity(String.class);
+
+		return gson.fromJson(sResponse, tradeCollectionType);
 	}
 }
